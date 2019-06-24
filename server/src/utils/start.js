3c92c1bbc2cf7b006d6eb0c400c2d1988af9ee70
Arman625101 <vip.arman2011@gmail.com>
2019-06-24 20:00:03 +0400

    testing heroku

--- server/src/utils/start.js
+++ server/src/utils/start.js
@@ -1,21 +1,16 @@
 const mongoose = require('mongoose');
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
-const {
-  DB_USER,
-  DB_PASS,
-  DB_HOST,
-  DB_PORT,
-  DB_NAME,
-  PORT = 3000,
-} = process.env;
+const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env.PORT;
 const socket = require('../socket');
 
 module.exports = app => {
   const http = require('http').createServer(app);
   const io = require('socket.io')(http);
   socket(io);
-  http.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
+  http.listen(process.env.PORT || 3000, () =>
+    console.log(`listening on http://localhost:${PORT}`),
+  );
 
   mongoose.connect(
     `mongodb://${DB_USER}:${DB_PASS}${DB_HOST}:${DB_PORT}/${DB_NAME}`,
