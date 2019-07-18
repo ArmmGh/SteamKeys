504ebea5776efbef8da3ec70450c4c4a034ba28a
Arman625101 <vip.arman2011@gmail.com>
2019-07-18 04:03:25 +0400

    test

--- server/src/index.js
+++ server/src/index.js
@@ -65,7 +65,13 @@ app.use(livedrop);
 app.use(games);
 app.use(cases);
 app.use(userbalance);
-
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.get('/', (req, res) => {
   res.send('LOL');
 });
