531c4992394df2622bebfb20ef0db2f8a2698cbd
Arman625101 <vip.arman2011@gmail.com>
2019-07-18 04:09:59 +0400

    test

--- server/src/index.js
+++ server/src/index.js
@@ -34,15 +34,7 @@ const url =
   process.env.NODE_ENV === 'development'
     ? `http://${host}${port}`
     : `https://${host}`;
-const allowedOrigins = [url, 'https://any-pay.org/'];
-
-app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+const allowedOrigins = [url, 'https://any-pay.org/', undefined];
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
