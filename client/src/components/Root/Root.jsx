6b46f3fe0661f6980cb9d4f20cb5611bfd07bdf0
Vrdo12 <stepanyan-vardan@list.ru>
2019-08-14 16:13:07 +0400

    pushing to live

--- client/src/components/Root/Root.jsx
+++ client/src/components/Root/Root.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import { BrowserRouter as Router } from 'react-router-dom';
 import io from 'socket.io-client';
+import { StateProvider } from '../../context';
 import fetchApi from '../../utils/fetchApi';
+import EN from '../../trans/en.json';
+import RU from '../../trans/ru.json';
 import App from '../App';
 // import '../../../71520c5957f682a7807934a73c201932.txt';
 
@@ -10,6 +13,21 @@ const Root = () => {
   const host = process.env.HOST || 'localhost';
   const port = process.env.PORT || ':3000';
   const ref = process.env.REF || 'http';
+
+  const storageLang = window.localStorage.getItem('lang');
+  const browserLang =
+    window.navigator.language.substring(0, 2) ||
+    window.navigator.userLanguage.substring(0, 2);
+  if (storageLang !== browserLang) {
+    window.localStorage.setItem('lang', browserLang);
+  }
+
+  const translations = {
+    en: EN,
+    ru: RU,
+  };
+  const getTranslate = langCode => key =>
+    translations[langCode === 'ru' ? 'ru' : 'ru'][key] || key;
 
   const url = window.location.origin.match('keyforu')
     ? 'https://steam-keys.herokuapp.com'
