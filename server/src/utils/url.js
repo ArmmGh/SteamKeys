ae609e891a6dcd507a2c456f623d52d78e485211
Vrdo12 <stepanyan-vardan@list.ru>
2019-08-17 23:23:39 +0400

    fixing login

--- server/src/utils/url.js
+++ server/src/utils/url.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 exports.returnURLSteam =
   process.env.NODE_ENV === 'development'
     ? `http://${host}:${port}/steam/return`
-    : `https://${host}/auth/steam/return`;
+    : `https://${host}/steam/return`;
 
 exports.defaultURLSteam =
   process.env.NODE_ENV === 'development'
@@ -16,7 +16,7 @@ exports.defaultURLSteam =
 exports.returnURLVk =
   process.env.NODE_ENV === 'development'
     ? `http://${host}:${port}/vkontakte/callback`
-    : `https://${host}/auth/vkontakte/callback`;
+    : `https://${host}/vkontakte/callback`;
 exports.defaultURLVk =
   process.env.NODE_ENV === 'development'
     ? `http://${host}:${port}/`
