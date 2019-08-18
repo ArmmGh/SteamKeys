7a0b712c8b4f352e6916b0991ddec230d61386a2
Vrdo12 <stepanyan-vardan@list.ru>
2019-08-19 00:51:31 +0400

    pushing to live

--- server/src/utils/url.js
+++ server/src/utils/url.js
@@ -5,8 +5,8 @@ const port = process.env.PORT || 3000;
 
 exports.returnURLSteam =
   process.env.NODE_ENV === 'development'
-    ? `http://${host}:${port}/auth/steam/return`
-    : `https://${host}/auth/steam/return`;
+    ? `http://${host}:${port}/steam/return`
+    : `https://${host}/steam/return`;
 
 exports.defaultURLSteam =
   process.env.NODE_ENV === 'development'
@@ -15,8 +15,8 @@ exports.defaultURLSteam =
 
 exports.returnURLVk =
   process.env.NODE_ENV === 'development'
-    ? `http://${host}:${port}/auth/vkontakte/callback`
-    : `https://${host}/auth/vkontakte/callback`;
+    ? `http://${host}:${port}/vkontakte/callback`
+    : `https://${host}/vkontakte/callback`;
 exports.defaultURLVk =
   process.env.NODE_ENV === 'development'
     ? `http://${host}:${port}/`
