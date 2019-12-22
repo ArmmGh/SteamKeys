940afc6b72922e358f44f99b15ab517b7823bd1a
Vrdo12 <stepanyan-vardan@list.ru>
2019-12-22 19:18:00 +0400

    pushing to live

--- client/src/components/Table/Table.jsx
+++ client/src/components/Table/Table.jsx
@@ -17,24 +17,6 @@ const Cases = ({ history }) => {
     window.localStorage.getItem('closeAlert'),
   );
 
-  function importAll(r) {
-    const images = {};
-    // eslint-disable-next-line array-callback-return
-    r.keys().map((item, index) => {
-      images[item.replace('./', '').replace(/\.(png|jpe?g|svg)$/, '')] = r(
-        item,
-      );
-    });
-    return images;
-  }
-
-  const images = importAll(
-    require.context('../../assets/profile', false, /\.(png|jpe?g|svg)$/),
-  );
-
-  const imagesCases = importAll(
-    require.context('../../assets/cases', false, /\.(png|jpe?g|svg)$/),
-  );
 
   const onShowMore = () => e => {
     if (showMore) {
@@ -114,137 +96,9 @@ const Cases = ({ history }) => {
 
   useEffect(() => {}, []);
 
-  // Autocomplete
-  function autocomplete(inp, arr) {
-    let currentFocus;
-    inp.addEventListener('input', function(e) {
-      let b;
-      const val = this.value;
-      closeAllLists();
-      if (!val) {
-        return false;
-      }
-      currentFocus = -1;
-      const a = document.createElement('DIV');
-      a.setAttribute('id', `${this.id}autocomplete-list`);
-      a.setAttribute('class', 'autocomplete-items');
-      this.parentNode.appendChild(a);
-      for (let i = 0; i < arr.length; i += 1) {
-        if (
-          arr[i].name.substr(0, val.length).toUpperCase() === val.toUpperCase()
-        ) {
-          b = document.createElement('DIV');
-          b.classList.add('search_item_holder');
-          b.innerHTML += `<img src="${images[arr[i].img]}" />`;
-          b.innerHTML += `<p>
-            <strong>${arr[i].name.substr(0, val.length)}</strong>
-            ${arr[i].name.substr(val.length)}
-          </p>`;
-          b.innerHTML += `<input type='hidden' value='${arr[i].name}'>`;
-          // eslint-disable-next-line no-loop-func
-          b.addEventListener('click', function() {
-            history.push(`/case/${arr[i].url}`);
-            inp.value = this.getElementsByTagName('input')[0].value;
-            closeAllLists();
-          });
-          a.appendChild(b);
-        }
-      }
-    });
-    inp.addEventListener('keydown', function(e) {
-      let x = document.getElementById(`${this.id}autocomplete-list`);
-      if (x) x = x.getElementsByTagName('div');
-      if (e.keyCode === 40) {
-        currentFocus += 1;
-        addActive(x);
-      } else if (e.keyCode === 38) {
-        currentFocus -= 1;
-        addActive(x);
-      } else if (e.keyCode === 13) {
-        e.preventDefault();
-        if (currentFocus > -1) {
-          if (x) x[currentFocus].click();
-        }
-      }
-    });
-    function addActive(x) {
-      if (!x) return false;
-      removeActive(x);
-      if (currentFocus >= x.length) currentFocus = 0;
-      if (currentFocus < 0) currentFocus = x.length - 1;
-      x[currentFocus].classList.add('autocomplete-active');
-    }
-    function removeActive(x) {
-      for (let i = 0; i < x.length; i++) {
-        x[i].classList.remove('autocomplete-active');
-      }
-    }
-    function closeAllLists(elmnt) {
-      const x = document.getElementsByClassName('autocomplete-items');
-      for (let i = 0; i < x.length; i++) {
-        if (elmnt !== x[i] && elmnt !== inp) {
-          x[i].parentNode.removeChild(x[i]);
-        }
-      }
-    }
-    document.addEventListener('click', e => {
-      closeAllLists(e.target);
-    });
-  }
-  if (document.getElementById('myInput')) {
-    autocomplete(document.getElementById('myInput'), games);
-  }
-
-  const closeAlert = () => e => {
-    toggleAlert(true);
-    window.localStorage.setItem('closeAlert', true);
-  };
-
   return (
     <div className="cases_holder">
       <div className="main-width">
-        <h1>{translate('keys')}</h1>
-        <ul className="ourKeys">
-          {otherCases.map((item, i) => (
-            <li key={i} className="item">
-              <Link to={`/case/${item.url}`} href={`/case/${item.url}`}>
-                <div className="image">
-                  <img src={imagesCases[item.img]} alt={item.name} />
-                </div>
-                <div className="info">
-                  <div className="name">
-                    {translate(item.name)} {translate('key')}
-                  </div>
-                  <div className="price">{item.priceRUB}₽</div>
-                </div>
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <div className="gameKeysHolder">
-          <h1>{translate('chooseGame')}</h1>
-          {!hideAlert && (
-            <div className="alert">
-              <div className="close" onClick={closeAlert()}>
-                <MdClose />
-              </div>
-              <div className="title">Внимание!!!</div>
-              <div className="text">
-                Нижние цены написано для одного открывания кейса
-              </div>
-            </div>
-          )}
-          <form className="search" autoComplete="off">
-            <div className="autocomplete">
-              <input
-                id="myInput"
-                type="text"
-                name="myCountry"
-                placeholder="Поиск игры..."
-              />
-            </div>
-          </form>
-          <div className="addtable">
           {benefit && (
             <table className="table" id="tbl">
               {benefit.map((item, index) => (
@@ -258,8 +112,6 @@ const Cases = ({ history }) => {
           )}
             </div>
         </div>
-      </div>
-    </div>
   );
 };
 
