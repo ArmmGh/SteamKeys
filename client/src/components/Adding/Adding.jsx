63a872c9b9aa137435eebbea4cc30d3429c58bf5
Vrdo12 <stepanyan-vardan@list.ru>
2019-12-23 22:32:03 +0400

    pushing to live

--- client/src/components/Adding/Adding.jsx
+++ client/src/components/Adding/Adding.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import log from '../../assets/profile/payeer-logo.png'
 import './Adding.scss';
 
 
 const Adding = () =>{
     return(
-        <h1>hello</h1>
+        <React.Fragment>
+        <div className="addcontainer">
+            <div className="alladd">
+        <div className="heading">
+            <h3>Пополнение баланса</h3>
+        </div>
+        <div className="paymethod">
+        <h5>вибирите метод попалнении</h5>
+        <img src={log} alt="payeer" />
+        </div>
+            </div>
+                </div>
+        </React.Fragment>
     )
 }
 
