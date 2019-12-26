5ad9709200179de4921f946ee14aba90c6f70055
Vrdo12 <stepanyan-vardan@list.ru>
2019-12-27 01:13:46 +0400

    pushing invest

--- client/src/components/Invest/Invest.jsx
+++ client/src/components/Invest/Invest.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import Menu from '../Menu/index';
+import invest from '../../assets/profile/invest.png';
 import './Invest.scss';
 
 
 const Invest = () =>{
     return(
         <React.Fragment>
-            <div>
             <Menu />
-            <p>hello</p>
+            <div className="investcontainer">
+                <div className="investall">
+                    <div className="investlog">
+                        <img src={invest} alt="invest" />
+                    </div>
+                </div>
             </div>
         </React.Fragment>
     )
