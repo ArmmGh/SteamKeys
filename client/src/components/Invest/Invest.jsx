13bdfefe52c19e69de3653e234f1194c9e28eead
Vrdo12 <stepanyan-vardan@list.ru>
2019-12-27 02:13:22 +0400

    invest 50%

--- client/src/components/Invest/Invest.jsx
+++ client/src/components/Invest/Invest.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import Menu from '../Menu/index';
 import invest from '../../assets/profile/invest.png';
 import './Invest.scss';
+import { useStateValue } from '../../context';
 
 
 const Invest = () =>{
+    const [{ user }] = useStateValue();
     return(
         <React.Fragment>
             <Menu />
@@ -12,6 +14,10 @@ const Invest = () =>{
                 <div className="investall">
                     <div className="investlog">
                         <img src={invest} alt="invest" />
+                    </div>
+                    <div className="txt">
+                        <p>Укажите сумму, которую хотите вкладивать</p>
+                        <span>Максимум: {user.balance}</span>
                     </div>
                 </div>
             </div>
