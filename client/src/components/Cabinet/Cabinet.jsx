9a96b98896512792c7542c86526df7d749437e7d
Vrdo12 <stepanyan-vardan@list.ru>
2019-12-28 00:01:45 +0400

    pushing cabinet

--- client/src/components/Cabinet/Cabinet.jsx
+++ client/src/components/Cabinet/Cabinet.jsx
@@ -9,7 +9,13 @@ const Cabinet = () =>{
     return(
         <React.Fragment>
             <Menu />
-            <p>hello</p>
+            <div className="cabinetcontainer">
+                <div className="cabinetall">
+                    <div className="head">
+                        <h1>Личний Кабинет</h1>
+                    </div>
+                </div>
+            </div>
         </React.Fragment>
     )
 }
