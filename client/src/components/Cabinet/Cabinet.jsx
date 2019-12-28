e08e6631d599e7d66eba34e0b246159b1e36c112
Vrdo12 <stepanyan-vardan@list.ru>
2019-12-28 17:31:52 +0400

    cabinet

--- client/src/components/Cabinet/Cabinet.jsx
+++ client/src/components/Cabinet/Cabinet.jsx
@@ -14,6 +14,7 @@ const Cabinet = () =>{
                     <div className="head">
                         <h1>Личний Кабинет</h1>
                     </div>
+                    <div className="inf">
                         <div className="formhead">
                         <h3>Данные аккаунта</h3>
                         </div>
@@ -30,6 +31,24 @@ const Cabinet = () =>{
                        <div><input type="text" /></div>
                     </form>
                     </div>
+                        </div>
+                    </div>
+                    <div className="paym">
+                    <div className="formhead">
+                        <h3>Платежние Данные</h3>
+                        </div>
+                    <div className="inputes">
+                    <div className="names">
+                        <div className="column">Payeer</div>
+                        <div className="column">Qiwi</div>
+                    </div>
+                    <div className="field">
+                    <form>
+                       <div><input type="text" /></div>
+                       <div><input type="text" /></div>
+                    </form>
+                    </div>
+                        </div>
                     </div>
                 </div>
             </div>
