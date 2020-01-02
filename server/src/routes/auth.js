c03d2b52a394bb9117989f7ae1c80374119d2131
Vrdo12 <stepanyan-vardan@list.ru>
2020-01-02 23:36:16 +0400

    mail

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -38,7 +38,7 @@ const mex = (res , next) =>{
 
 auth.get('/steam', passport.authenticate('steam'));
 
-auth.get('/mail/callback', passport.authenticate('mail', {failureRedirect: `${url}`}),(req,res,next) =>{
+auth.get('/mail/callback/storage', passport.authenticate('mail', {failureRedirect: `${url}`}),(req,res,next) =>{
   const date = req.query.code
   const url1 = "https://oauth.mail.ru/token?client_id=3c4c8430046f410d9aa30a07bac55bad&client_secret=157d036e926043f3bed67151aaadbf71&code="
   const expert = url1.concat(date);
@@ -86,6 +86,25 @@ auth.get('/vkontakte', passport.authenticate('vkontakte'));
 
 auth.get('/vkontakte/callback',
   passport.authenticate('vkontakte', {failureRedirect: `${url}` ,
+  }),
+  (req, res, next) => {
+    const data = {
+      username: req.user.displayName,
+      userID: req.user.id,
+      profileurl: req.user.profileUrl,
+      imgurl: req.user._json.photo,
+      ip: req.ipInfo,
+    };
+    db.update(data);
+    res.redirect(`${url}`);
+    next();
+  },
+);
+
+auth.get('/mail', passport.authenticate('mailru'));
+
+auth.get('/mail/callback',
+  passport.authenticate('mailru', {failureRedirect: `${url}` ,
   }),
   (req, res, next) => {
     const data = {
