93a95df75cb26cc5a0fa29483297b210cb0d2b5f
Vrdo12 <stepanyan-vardan@list.ru>
2020-01-03 02:30:39 +0400

    mail

--- server/src/index.js
+++ server/src/index.js
@@ -15,7 +15,7 @@ const cors = require('cors');
 const cookieSession = require('cookie-session');
 const cookieParser = require('cookie-parser');
 const setup = require('./utils/start');
-require('./utils/passport-auth');
+// require('./utils/passport-auth');
 require('./utils/passport-steam');
 require('./utils/passport-vk');
 
