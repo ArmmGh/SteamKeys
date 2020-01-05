ae89db1fe0d9c78c1fe96b2e650600e919f8f9f3
Vrdo12 <stepanyan-vardan@list.ru>
2020-01-06 00:38:53 +0400

    modal in add

--- client/src/components/Adding/Adding.jsx
+++ client/src/components/Adding/Adding.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Modal from 'react-modal';
 import Menu from '../Menu/index';
 import Table from '../Table/index';
 import log from '../../assets/profile/payeer-logo.png';
@@ -11,14 +12,61 @@ import './Adding.scss';
 
 
 const Adding = () =>{    
-    const alert = () => e =>{
+    const [amount, setAmount] = useState(100);
+    const [isActive, setActive] = useState(false);
+    const [modalIsOpen, setModal] = useState(false);
+    const [ { user } ] = useStateValue();
+
+    const openModal = () => e => {
+        setModal(true);
+        setSum('');
+      };
+
+      const alert = () => e =>{
         console.log("hello");
     }
-    const [
-        { user }
-      ] = useStateValue();
+
+      const handeleChange = val => {
+        if (val.match(/^[0-9]+$/)) {
+          setAmount(val);
+        }
+      };
     return(
         <React.Fragment>
+        <Modal
+        closeTimeoutMS={200}
+        ariaHideApp={false}
+        onRequestClose={() => setModal(false)}
+        shouldCloseOnOverlayClick={true}
+        isOpen={modalIsOpen}
+        className="Modal"
+        overlayClassName="OverlayHeader"
+      >
+        <div className="header">
+          <div />
+          <h1>ПОПОЛНЕНИЕ БАЛАНСА</h1>
+          <div className="close">
+            <MdClose onClick={() => setModal(false)} />
+          </div>
+        </div>
+        <div className="body">
+          <h1>Введите сумму</h1>
+          <div className="inpHolder">
+            <form action="https://any-pay.org/merchant" method="post">
+              <input type='hidden' name="merchant_id" defaultValue="4183" id="merchant_id" />
+              <input type='hidden' name="secret_key" defaultValue={process.env.api_key} id="secret_key" />
+              <input type='hidden' name="pay_id" defaultValue={user.userID} id="pay_id"/>'
+              <input type='text' name="amount" value={sum} onChange={e => handeleChange(e.target.value)} id="amount"/>
+              <button type="submit">
+                Пополнить
+              </button>
+            </form>
+          </div>
+          <div className="info">
+            Средства приходят моментально, но могут быть задержки до 5-10 минут.
+          </div>
+        </div>
+      </Modal>
         <div className="addcontainer">
             <div className="alladd">
                 <Menu />
@@ -30,10 +78,10 @@ const Adding = () =>{
             <p>Укажите сумму и способ пополнения</p>
             </div>
             <div className="suminput">
-                <input type="text" defaultValue={user.balance}/>
+                <input type="text" value={amount} onChange={e => handeleChange(e.target.value)} />
             </div>
         <div className="imgholder">
-            <button onClick={alert()}><img src={log} alt="payeer" /></button>
+            <button onClick={openModal()}><img src={log} alt="payeer" /></button>
         </div>
         </div>
         <div className="tabl">
--- client/src/components/Invest/Invest.jsx
+++ client/src/components/Invest/Invest.jsx
@@ -39,6 +39,40 @@ const Invest = () =>{
     }
     return(
         <React.Fragment>
+      <Modal
+        closeTimeoutMS={200}
+        ariaHideApp={false}
+        onRequestClose={() => setModal(false)}
+        shouldCloseOnOverlayClick={true}
+        isOpen={modalIsOpen}
+        className="Modal"
+        overlayClassName="OverlayHeader"
+      >
+        <div className="header">
+          <div />
+          <h1>ПОПОЛНЕНИЕ БАЛАНСА</h1>
+          <div className="close">
+            <MdClose onClick={() => setModal(false)} />
+          </div>
+        </div>
+        <div className="body">
+          <h1>Введите сумму</h1>
+          <div className="inpHolder">
+            <form action="https://any-pay.org/merchant" method="post">
+              <input type='hidden' name="merchant_id" defaultValue="4183" id="merchant_id" />
+              <input type='hidden' name="secret_key" defaultValue={process.env.api_key} id="secret_key" />
+              <input type='hidden' name="pay_id" defaultValue={user.userID} id="pay_id"/>'
+              <input type='text' name="amount" value={sum} onChange={e => handeleChange(e.target.value)} id="amount"/>
+              <button onClick={storeData()} type="submit">
+                Пополнить
+              </button>
+            </form>
+          </div>
+          <div className="info">
+            Средства приходят моментально, но могут быть задержки до 5-10 минут.
+          </div>
+        </div>
+      </Modal>
             <Menu />
             <div className="investcontainer">
                 <div className="investall">
